0f9ab007568090235cce1be7dc9318db326db7be
Biniam batu <Biniambatu12@gmail.com>
2024-12-05 20:39:20 +0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
 import Header from './Header';
 import RecommendedVideos from './RecommendedVideos';
+import SearchPage from './SearchPage';
 import SideBar from './SideBar';
-
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
+
     <div className="App">
-      <Header/>
-      <div className='app__page'>
-        <SideBar />
-        <RecommendedVideos />
-      </div>
+    
+         <Routes>
+          <Route path='/search' element={<SearchPage/>}> 
+           {/* <Header/> */}
+           {/* <h1>Search page</h1> */}
+           </Route>
+           <Route path=''></Route>
+           {/* <Route path='/' element={ } /> */}
+          <Route path='/'> 
+          {/* <Header/>  */}
+           <div className='app__page'>
+            <SideBar />
+            <RecommendedVideos /> 
+           </div>
+          </Route>
+        </Routes>
+     
     </div>
   );
 }
